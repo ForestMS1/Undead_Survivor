5be4c7578dddcb5d22570a013eb9c30add5fb6ef
MsKing <dasung56@gmail.com>
2024-10-11 23:13:19 +0900

    Player 이동방향 바라보기

--- Assets/Undead Survivor/Scripts/Player.cs
+++ Assets/Undead Survivor/Scripts/Player.cs
@@ -9,11 +9,13 @@ public class Player : MonoBehaviour
     public Vector2 inputVec;
     Rigidbody2D rigid;
     public float speed;
+    SpriteRenderer spriter;
 
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
     }
 
     //물리 연산 프레임마다 호출되는 생명주기 함수
@@ -27,5 +29,14 @@ public class Player : MonoBehaviour
     void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+    }
+
+    //프레임이 종료 되기 전 실행되는 생명주기 함수
+    void LateUpdate()
+    {
+        if(inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
