97a62f09e06a31a121c2c48bf2366399dae9a0f5
MsKing <dasung56@gmail.com>
2024-10-11 23:03:41 +0900

    Player Input 컴포넌트 & 프로필 에셋으로 플레이어 이동

--- Assets/Undead Survivor/Scripts/Player.cs
+++ Assets/Undead Survivor/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -15,19 +16,16 @@ public class Player : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        inputVec.x = Input.GetAxisRaw("Horizontal"); //project에서 수평 설정된 이름
-        inputVec.y = Input.GetAxisRaw("Vertical"); //project에서 수직 설정된 이름
-    }
-
     //물리 연산 프레임마다 호출되는 생명주기 함수
     void FixedUpdate()
     {
-        //대각선 이동(루트2)도 상하좌우 똑같게(1) 이동하도록 normalized
-        Vector2 nextVector = inputVec.normalized * speed * Time.fixedDeltaTime;
+        Vector2 nextVector = inputVec * speed * Time.fixedDeltaTime;
         // 위치 이동
         rigid.MovePosition(rigid.position + nextVector); // MovePosition은 월드좌표기준이라 현재좌표값을 더해줌
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
     }
 }
