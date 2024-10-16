f54ffce5b6de58d5c4bad55707f2a42d2d6f123d
MsKing <dasung56@gmail.com>
2024-10-17 00:27:40 +0900

    몬스터 이동 구현

---
+++ Assets/Undead Survivor/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D target; //眠利鸥百
+
+    bool isLive = true; //积粮咯何
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        if(!isLive)
+        {
+            return;
+        }
+        Vector2 dirVec = target.position - rigid.position;
+        Vector2 nextVect = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVect);
+        rigid.velocity = Vector2.zero;
+    }
+
+    void LateUpdate()
+    {
+        if (!isLive)
+        {
+            return;
+        }
+        spriter.flipX = target.position.x < rigid.position.x;
+    }
+}
